43af64adf61de8409f3f9f0b198be56ec0dd06c2
cyb3rkun <shadowreaper3437@gmail.com>
2024-10-06 20:23:28 +0000

    removed extra trash

--- lua/CYB3RKUN/init.lua
+++ lua/CYB3RKUN/init.lua
@@ -1,9 +1,3 @@
 require('CYB3RKUN.remap')
-<<<<<<< Updated upstream
 require('CYB3RKUN.set')
---require('CYB3RKUN.attachLSP')
-=======
-require('CYB3RKUN.vim-settings')
-require('CYB3RKUN.attachLSP')
->>>>>>> Stashed changes
 
