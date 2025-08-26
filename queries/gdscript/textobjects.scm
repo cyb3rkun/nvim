d605101de0ff024a94015f3909907843d8613698
cyb3rkun <cyb0rg@duck.com>
2025-08-26 07:18:32 +0200

    add custom treesitter queries for gdscript

--- queries/gdscript/textobjects.scm
+++ queries/gdscript/textobjects.scm
@@ -4,3 +4,46 @@
 
 (_
 (type) @type)
+
+(class_definition
+  body: (body(_))@class.inner)
+
+(variable_statement
+(name) @name)
+
+(_
+  (name)@name)
+
+; TODO: Find out what is overriding and kill it hard!
+(_
+  (
+   (function_definition)
+   (comment)
+  ) @function.outer)
+
+(assignment
+  left: (subscript (identifier)@name))
+
+(_
+  left: (identifier) @name)
+
+(_
+ (type) @type.inner)
+
+(_
+ (type) @type.outer)
+
+(_
+  (attribute_call) @call.outer)
+
+(source
+  (comment)+ @comment.outer)
+(
+ (enum_definition) @enum)
+
+(assignment
+  left: (subscript) @assignment.lhs)
+
+(assignment
+  left: (attribute) @assignment.lhs)
+
